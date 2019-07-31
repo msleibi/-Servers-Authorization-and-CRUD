8cc883a92ea93f5c090848e90440008da1dad851
Mohammad Sleibi <msalibi83@gmail.com>
2019-07-31 21:22:49 +0200

    feat: New Database
    Create new database 'restaurantmenuwithusers'
    
    by modifying the specified files and execute lotsofmenusUsers.py

--- database_setup.py
+++ database_setup.py
@@ -6,24 +6,48 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
  
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
  
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+       #Returns object data in easily serializeable format
+       return {
+           'name'         : self.name,
+           'id'         : self.id,
+           'user_id'    : self. user_id
+           
+       }
  
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
 
-    name =Column(String(80), nullable = False)
+    name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant) 
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
 
 #We added this serialize function to be able to send JSON objects in a serializable format
     @property
@@ -38,7 +62,7 @@ class MenuItem(Base):
        }
  
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
